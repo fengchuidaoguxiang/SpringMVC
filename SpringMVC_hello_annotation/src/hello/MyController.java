a3db3b8d88a8641953e23acf5299ae1f16097b57
fengchuidaoguxiang <33154249+fengchuidaoguxiang@users.noreply.github.com>
2019-01-07 21:21:50 +0800

    更新

---
+++ SpringMVC_hello_annotation/src/hello/MyController.java
@@ -0,0 +1,30 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@Controller //将这个Bean组件注册到Spring的IoC容器中
+public class MyController {
+
+    @RequestMapping("/hello4")
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("----输出视图-----");
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("/WEB-INF/views/myView.jsp");
+        modelAndView.addObject("msg","使用注解开发SpringMVC");
+        return modelAndView;
+    }
+    @RequestMapping("/hello5")
+    public ModelAndView handleRequest5(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("----输出视图2-----");
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("/WEB-INF/views/myView.jsp");
+        modelAndView.addObject("msg","使用注解开发SpringMVC2");
+        return modelAndView;
+    }
+}
---
+++ webapp/WEB-INF/classes/artifacts/SpringMVC_hello_annotation_war_exploded/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Admin
+  Date: 2019/1/7
+  Time: 20:14
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
